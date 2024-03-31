4cb0c3e24d2bb24020a54cfb6e6787f1886c334c
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-03-31 04:28:25 -0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::fs;
+use walkdir::WalkDir;
+use uuid::Uuid;
+use std::io::Read;
+use std::io::Write;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: renamejpgs <directory> <directory>");
+        return;
+    }
+
+    let apath = &args[1];
+
+    for e in WalkDir::new(apath)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        if e.metadata().unwrap().is_file() {
+            let fname = e.path().to_string_lossy().to_string();
+            if fname.contains("pussy") {
+                fs::remove_file(&fname).expect("Unable to remove file");
+            } else {
+                let auuid = Uuid::new_v4();
+                let ext_split = fname.split(".").collect::<Vec<&str>>();
+                let mut ext = ext_split.last().unwrap().to_string().to_lowercase();
+                if ext == "jpeg".to_string() {
+                    ext = "jpg".to_string();
+                };
+                let out_path = format!("{}/{}.{}", &args[2], auuid, ext);
+                println!("{} ->\n {}", fname.clone(), out_path);
+                let mut f = std::fs::File::open(fname.clone()).unwrap();
+                let mut buffer = Vec::new();
+                f.read_to_end(&mut buffer).unwrap();
+                let mut f = std::fs::File::create(out_path).unwrap();
+                f.write_all(&buffer).unwrap();
+                fs::remove_file(fname.clone()).expect("Unable to remove file");
+            };
+        }
+    }
+}
